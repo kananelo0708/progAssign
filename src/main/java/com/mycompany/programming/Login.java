9ea34873b68a255e1534e7805c35bf72f73243b1
kani070820 <115329645+Kopanomokhai538@users.noreply.github.com>
2026-09-17 15:43:12 +0200

    modified the the message of when the wrong info is added

--- src/main/java/com/mycompany/programming/Login.java
+++ src/main/java/com/mycompany/programming/Login.java
@@ -127,7 +127,7 @@ public class Login {
             return "Welcome " + firstName + ", " + lastName
                  + " it is great to see you again.";
         } else {
-            return "Username or password incorrect, please try again.";
+            return "Username or password incorrect, please try again access your account.";
         }
     }
 }
